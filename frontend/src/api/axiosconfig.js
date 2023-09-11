7141b66bd85733c7ce492ab04f92e7d2def6e78b
SuperDem0u <superdem0u@mailfence.com>
2023-09-11 13:47:39 -0300

    ngrokUpdate

--- frontend/src/api/axiosconfig.js
+++ frontend/src/api/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://08f5-177-184-180-19.ngrok-free.app', // Paste your link here
+    baseURL:'https://6691-177-184-189-178.ngrok-free.app', // Paste your link here
     headers: {"ngrok-skip-browser-warning": "true"} // Anyone alteration here please
 });
